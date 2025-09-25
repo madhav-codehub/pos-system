4ccf034983eccc64c23085b84e159463054ddaea
madhav-codehub <madhav.p.akv@gmail.com>
2025-09-25 20:34:40 +0530

    adding git ignore file

---
+++ src/test/java/com/creativehub/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
